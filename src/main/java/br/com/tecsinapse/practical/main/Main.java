92d6a6e33a0a05bd246feca9a67fd0167ec33dae
filipe_nevola <filipe.nevola@tecsinapse.com.br>
2012-12-27 12:49:00 -0200

    texto explicativo

--- src/main/java/br/com/tecsinapse/practical/main/Main.java
+++ src/main/java/br/com/tecsinapse/practical/main/Main.java
@@ -7,6 +7,43 @@ import br.com.tecsinapse.practical.modelo.Pedido;
 
 import com.google.common.collect.Lists;
 
+/**
+ * Você está recebendo um projeto que contém as classes ItemPedido e Pedido. Um
+ * Pedido contém o cnpj do cliente, o usuário solicitante e uma lista de itens.
+ * Um ItemPedido contém também o cnpj do cliente, o usuário solicitante, o
+ * código do item e a quantidade solicitada. Você deve:
+ * 
+ * 1 - Criar um atributo chamado valorTotal, tanto na classe ItemPedido como na
+ * classe Pedido. Utilize o tipo correto no atributo levando em conta que
+ * trata-se de um valor monetário com duas casas decimais;
+ * 
+ * 2 - Criar um atributo chamado valorUnitario na classe ItemPedido, que deverá
+ * ser calculado dividindo-se o valorTotal pela quantidade e arredondando-se em
+ * duas casas decimais pelo padrão monetário de arredondamento (0-4 pra baixo,
+ * 5-9 para cima). Escolha o tipo mais apropriado levando-se em conta que é um
+ * valor monetário precico;
+ * 
+ * 3 - Implementar o método gerarPedidosAgrupados(List<ItemPedido>) :
+ * List<Pedido> na classe Teste passada, levando em conta que deve ser gerado um
+ * único pedido para cada par único de CNPJ de cliente e usuário solicitante.
+ * Não devem haver dois itens no mesmo pedido com o mesmo código de item -
+ * quando isso acontecer, deve-se somar as quantidades e o valor total, gerando
+ * um único item consolidado.
+ * 
+ * Além dos critérios obrigatórios acima, serão dados pontos adicionais se a
+ * ordem dos pedidos e dos itens for mantida durante o processo (dar um
+ * exemplo).
+ * 
+ * Sugira também (sem alterar o código) como o modelo poderia ser melhorado
+ * usando apenas classes do Java SE 7 para que ficasse mais correto.
+ * 
+ * Não é permitido alterar a assinatura de nenhum método público exposto nas
+ * classes, inclusive os construtores padrões. Pode-se adicionar outros métodos
+ * além dos requeridos acima para facilitar a implementação.
+ * 
+ * @author TecSinapse
+ * 
+ */
 public class Main {
 
 	public static List<Pedido> gerarPedidosAgrupados(List<ItemPedido> itens) {
